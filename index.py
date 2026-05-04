1ef6515362dbd5bc30a064738c1e5cba82b17418
carlosluizalves-rentalAI <carlosluiz.alves@gmail.com>
2026-05-04 16:36:08 -0300

    Diagnóstico avançado e bibliotecas Vercel

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-from app import app as application
+try:
+    from app import app as application
+except Exception as e:
+    import traceback
+    print("ERRO CRÍTICO NA INICIALIZAÇÃO DO APP:")
+    print(traceback.format_exc())
+    raise e
